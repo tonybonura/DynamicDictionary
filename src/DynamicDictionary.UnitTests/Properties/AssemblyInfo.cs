7a87e4ee6c0c2ea54cd9ab4e52de03d7da265677
Tony Bonura <tony@bonura.net>
2017-01-09 11:35:01 -0600

    replaced nunit test with default VS test harness

--- src/DynamicDictionary.UnitTests/Properties/AssemblyInfo.cs
+++ src/DynamicDictionary.UnitTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DynamicDictionaryTests")]
+[assembly: AssemblyTitle("DynamicDictionary.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DynamicDictionaryTests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("DynamicDictionary.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("90bdf2c5-97ab-4938-99d0-9bc53cc887f4")]
+[assembly: Guid("5c6a7ff2-3c4d-47ca-814b-c601e260220e")]
 
 // Version information for an assembly consists of the following four values:
 //
